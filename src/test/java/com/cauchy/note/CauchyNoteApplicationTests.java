cdc4e7fc313686d34bfb4ccd1242d94d2d1b612b
gitCauchy <mailcauchy@gmail.com>
2019-12-05 21:37:16 +0800

    项目初始化

---
+++ src/test/java/com/cauchy/note/CauchyNoteApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cauchy.note;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CauchyNoteApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
