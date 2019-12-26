3dc9d71d43e2d338f1cfb6819713e06422c72c6d
gitCauchy <mailcauchy@gmail.com>
2019-12-26 16:49:10 +0800

    Springboot启动类

--- src/main/java/com/cauchynote/CauchyNoteApplication.java
+++ src/main/java/com/cauchynote/CauchyNoteApplication.java
@@ -1,10 +1,8 @@
 package com.cauchynote;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@MapperScan("com.cauchynote.*.mapper")
 @SpringBootApplication
 public class CauchyNoteApplication {
 
